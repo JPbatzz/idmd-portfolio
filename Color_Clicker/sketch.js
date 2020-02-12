a85571e24e62e6b00a88a71a1c7eb516bfb89d02
JPbatzz <32466317+JPbatzz@users.noreply.github.com>
2020-02-12 10:03:15 -0800

    Add files via upload
    v1

---
+++ Color_Clicker/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(500, 500);
+  background(255, 0, 0);
+}
+function draw() {
+  ellipse(mouseX, mouseY, 25, 25);
+if (mouseIsPressed) {
+  fill(random(255),random(255),random(255))
+ 
+}
+}
