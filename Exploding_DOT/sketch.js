efa9d1196fcf8be6e6fade38b3868cb5ce7ca5db
JPbatzz <32466317+JPbatzz@users.noreply.github.com>
2020-02-12 10:07:35 -0800

    Add files via upload
    v1

---
+++ Exploding_DOT/sketch.js
@@ -0,0 +1,31 @@
+var x = 250;
+var y = 250;
+var w = 50;
+function setup() {
+  createCanvas(500, 500);
+  background(255);
+  noStroke();
+}
+function draw() {
+  background(255);
+  fill(w/2,0,0);
+  ellipse(x, y, w, w);
+  distance = dist(x, y, mouseX, mouseY);
+  if (distance < w / 2) {
+    //   V - changes speed
+    w += 2;
+  } else {
+    //   V - changes original width
+    w = 5;
+    
+    
+if (w <= 500) {
+  } else {  
+  background(random(255),random(255),random(255))
+
+  
+
+}
+ 
+  
+  }}
