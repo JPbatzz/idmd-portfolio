be74d054c8a4ee07539558669f5844a1bdbcd72e
JPbatzz <32466317+JPbatzz@users.noreply.github.com>
2020-03-16 11:35:25 -0700

    Add files via upload

---
+++ Font/sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+  textSize(40);
+  textFont("Cinzel");
+}
+//Cinzel font from google fonts
+function draw() {
+  background(220);
+  text("Hello <3", 100, 200);
+}
